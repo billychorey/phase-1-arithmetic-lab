a37944edf7ef609aca47c205e2c3c992f9274026
Billy <billychorey@gmail.com>
2023-07-19 13:41:54 -0400

    math exercises

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+//multiply
+let num1, num2, multiply;
+num1 = 8;
+num2 = 8;
+multiply = num1 * num2 - 2;
+
+//ramdom
+let random = (Math.floor(Math.random() * 10));
+
+//mod
+let num3  = 36;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+//max
+let max = Math.max(5, 10, 15, 20);
